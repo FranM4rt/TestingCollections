5587c389cb0a66b7fd85968f8735b8ae35b642c0
FranM4rt <mjuanfrancisco45@gmail.com>
2024-04-04 14:14:57 -0300

    Add project files.

---
+++ StackTest/StackGenericTest.cs
@@ -0,0 +1,11 @@
+namespace StackTest
+{
+    public class StackGenericTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ StackTest/StackNoGenericTest.cs
@@ -0,0 +1,11 @@
+namespace StackTest
+{
+    public class StackNoGenericTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
