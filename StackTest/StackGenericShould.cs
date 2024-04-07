5cf4cb1ac53acf8bfba79687002620bca9acaa5e
FranM4rt <mjuanfrancisco45@gmail.com>
2024-04-06 21:09:23 -0300

    Creando varios test para la clase generica y uno para la no generica

--- StackTest/StackGenericShould.cs
+++ StackTest/StackGenericShould.cs
@@ -1,11 +1,80 @@
+using FluentAssertions;
+
 namespace StackTest
 {
     public class StackGenericShould
     {
         [Fact]
-        public void Test1()
+        public void CountNumberOfElementWithCount()
         {
-            
+            // Arrange
+            Stack<string> strings = new Stack<string>();
+
+            // Act
+            strings.Push("A");
+            strings.Push("B");
+            strings.Push("C");
+            strings.Push("D");
+            int count = strings.Count();
+
+            // Assert
+            count.Should().Be(4);
+        }
+
+        [Fact]
+        public void StoreSameTypeDataUsingPush()
+        {
+            // Arrange
+            Stack<string> strings = new Stack<string>();
+
+            // Act
+            strings.Push("!");
+            strings.Push("Mundo");
+            strings.Push("Hola");
+
+            // Assert
+            strings.Should().NotBeEmpty()
+                .And.HaveCount(3)
+                .And.ContainInOrder("Hola", "Mundo", "!");
+        }
+
+        [Fact]
+        public void RemoveAndReturnBeginningDataUsingPop()
+        {
+            // Arrange
+            Stack<string> strings = new Stack<string>();
+
+            // Act
+            strings.Push("4");
+            strings.Push("B");
+            string result = strings.Pop();
+
+            // Assert
+            result.Should().NotBeEmpty()
+                .And.NotBe("A")
+                .And.Be("B");
+            strings.Should().NotBeEmpty()
+                .And.HaveCount(1)
+                .And.ContainSingle("A");
+        }
+
+        [Fact]
+        public void ReturnBeginningDataUsingPeekWithoutRemoveIt()
+        {
+            // Arrange
+            Stack<string> strings = new Stack<string>();
+
+            // Act
+            strings.Push("A");
+            strings.Push("B");
+            string result = strings.Peek();
+
+            // Assert
+            result.Should().NotBeEmpty()
+                .And.Be("B");
+            strings.Should().NotBeEmpty()
+                .And.HaveCount(2)
+                .And.ContainInOrder("B", "A");
         }
     }
 }
--- StackTest/StackNoGenericShould.cs
+++ StackTest/StackNoGenericShould.cs
@@ -1,11 +1,24 @@
+using FluentAssertions;
+using System.Collections;
+
 namespace StackTest
 {
     public class StackNoGenericShould
     {
         [Fact]
-        public void Test1()
+        public void StoreDifferentTypeDataUsingPush()
         {
+            // Arrange
+            Stack data = new Stack();
 
+            // Act
+            data.Push(1);
+            data.Push("Hello");
+            data.Push(3.14);
+
+            // Assert
+            data.Should().NotBeNull()
+                .And.BeOfType<Stack>();
         }
     }
 }
